ddf076c0e0df858e655e687c611a2a10d60a2888
steven <steven@stevendeMacBook-Air.local>
2022-10-10 22:47:34 +0800

    src

--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,19 @@
 #include "euler_characteristic.h"
+#include "edges.h"
+#include <iostream>
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
   // ADD YOUR CODE HERE
+  int f = F.rows();
+  int v = F.maxCoeff()+1;
+  
+  std::cout<<"f:"<<f<<std::endl;
+  std::cout<<"v:"<<v<<std::endl;
+
+  Eigen::MatrixXi E = edges(F);
+  int e = E.rows();
+  Chi = v - e + f;
   return Chi;
 }
